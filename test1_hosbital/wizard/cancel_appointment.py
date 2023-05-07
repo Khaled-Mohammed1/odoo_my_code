c43c021ae96c19870665f8463499583df88ca15d
Khaled-Mohammed1 <132020872+Khaled-Mohammed1@users.noreply.github.com>
2023-05-07 05:02:13 +0300

    win 7/5/2023

---
+++ test1_hosbital/wizard/cancel_appointment.py
@@ -0,0 +1,30 @@
+from odoo import api, fields, models
+import datetime
+
+
+class CancelAppointmentWizard(models.TransientModel):
+    # مديول مش بيخزن داتا في الداتا بيز
+
+    # _name="الاسم التقني الخاص بالمديول"
+    _name = "cancel.appointment.wizard"
+    # _description="الاسم اللي هيظهر او الوصف "
+    _description = "Cancel Appointment Wizard"
+
+    # هنا بعمل انهارت اوفر رايت علي defualt get عشان اخلي الوقت يتاخد تلقائي
+    @api.model
+    def default_get(self, fields):
+        res = super(CancelAppointmentWizard, self).default_get(fields)
+        res['date_cancel'] = datetime.date.today()
+        return res
+
+    # many2one فيلد
+    appointment_id = fields.Many2one('hosbital.appointment', string="Appointment Wizard")
+    # فيلد من نوع text
+    reason = fields.Text(string="Reason")
+    # فيلد من نوع داتا
+    date_cancel = fields.Date(string="Cancellation Date")
+
+    # الزرار اللي عاملينه في الفيو في الفوتر
+    def action_cancel(self):
+        return
+#     هنا معرفينه مبيعملش حاجه
