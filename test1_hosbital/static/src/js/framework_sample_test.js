f2b9e097eff3c7377fbfdf0e0c2493b1b06c9dac
Khaled-Mohammed1 <132020872+Khaled-Mohammed1@users.noreply.github.com>
2023-05-20 21:44:26 +0300

    js test 20-5-2023

--- test1_hosbital/static/src/js/framework_sample_test.js
+++ test1_hosbital/static/src/js/framework_sample_test.js
@@ -1,2 +1,13 @@
-alert("loaded!!!!")
-console.log("loaded again in console ")
+//alert("loaded!!!!");
+let title = 'KHALED_MATRIX Title', description = 'description test', date = '9/12/1994';
+let test1 = `
+<div class="card">
+    <H3>Hello ${title}</H3>
+    <p>${description}</p>
+    <span>${date}</span>
+</div>
+`;
+
+document.write(test1);
+console.log(title);
+
