fddebeac3323697d13820b1931a62548f170b163
Khaled-Mohammed1 <132020872+Khaled-Mohammed1@users.noreply.github.com>
2023-05-30 11:36:11 +0300

    Whatsapp API

---
+++ whatsaapLoop/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'WhatsApp Loop',
+    'version': '15.0',
+    'summary': """
+        CRM inherit""",
+    'description': """
+        inherit CRM
+    """,
+    'category': 'inherit',
+    'author': 'K_MATRIX',
+    'website': "https://www.linkedin.com/in/khaled-amer-539422246/",
+    'depends': ['crm'],
+    'data': [
+        'views/crm_lead_view.xml'
+    ],
+    'demo': [],
+    'sequence': '-88',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+    },
+
+}
