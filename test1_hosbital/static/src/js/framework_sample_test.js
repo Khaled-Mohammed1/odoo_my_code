209e02329593982bfbda5ba0a7716e3d7733f99f
Khaled-Mohammed1 <132020872+Khaled-Mohammed1@users.noreply.github.com>
2023-07-14 02:40:12 +0300

    Update framework_sample_test.js

--- test1_hosbital/static/src/js/framework_sample_test.js
+++ test1_hosbital/static/src/js/framework_sample_test.js
@@ -1,10 +1,9 @@
 //alert("loaded!!!!");
-let title = 'KHALED_MATRIX Title', description = 'description test', date = '9/12/1994';
+let title = 'KHALED_MATRIX', description = 'Welcome Back ';
 let test1 = `
 <div class="card">
     <H3>Hello ${title}</H3>
     <p>${description}</p>
-    <span>${date}</span>
 </div>
 `;
 
